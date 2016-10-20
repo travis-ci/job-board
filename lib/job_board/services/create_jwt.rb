8de2414e92468dc0b3890cabab2bcbd29d10dbb5
Dan Buch <dan@travis-ci.org>
2016-10-21 01:15:35 +0200

    Move some once-only work out of a hot path

--- lib/job_board/services/create_jwt.rb
+++ lib/job_board/services/create_jwt.rb
@@ -6,6 +6,16 @@ require_relative 'service'
 module JobBoard
   module Services
     class CreateJWT < Service
+      class << self
+        def private_key
+          @private_key ||= Base64.decode64(JobBoard.config.jwt_private_key)
+        end
+
+        def job_max_duration
+          @job_max_duration ||= Integer(JobBoard.config.job_max_duration)
+        end
+      end
+
       def initialize(job_id: '')
         @job_id = job_id
         @alg = 'RS512'
@@ -21,12 +31,12 @@ module JobBoard
         {
           iss: "job-board/#{JobBoard.version}",
           sub: job_id,
-          exp: (Time.now.utc + Integer(JobBoard.config.job_max_duration)).to_i
+          exp: (Time.now.utc + self.class.job_max_duration).to_i
         }
       end
 
       def private_key
-        Base64.decode64(JobBoard.config.jwt_private_key)
+        self.class.private_key
       end
     end
   end
